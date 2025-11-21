851726a584592c68705489e38aec88887f06f08e
hsu330653-tech <hsu330653@gmail.com>
2025-11-21 03:17:42 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import joblib
 
 app = Flask(__name__)
 
@@ -18,9 +19,11 @@ def dbs():
 
 @app.route("/dbs_prediction",methods=["GET","POST"])
 def dbs_prediction():
-    q=request.form.get("q")
+    q=float(request.form.get("q"))
     print(q)
-    return(render_template("dbs_prediction.html"))
+    model = joblib.load("dbs.pkl")
+    r = model.predict([[q]])
+    return(render_template("dbs_prediction.html",r=r[0][0]))
     
 if __name__ == "__main__":
     app.run()
